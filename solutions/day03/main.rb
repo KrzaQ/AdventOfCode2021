618e730a2175ec6c2f58704394c536f13c170582
KrzaQ <krzaq@krzaq.cc>
2021-12-03 07:03:08 +0100

    Day 03 code cleanup.

--- solutions/day03/main.rb
+++ solutions/day03/main.rb
@@ -3,30 +3,29 @@
 DATA = File.read('data.txt').split
 
 PART1 = DATA.map(&:chars).transpose.map(&:join).map do
-    [ _1.count(?0), _1.count(?1) ]
-end.map do
-    _1 > _2 ? [ ?0, ?1 ] : [ ?1, ?0 ]
+    _1.count(?0) > _1.count(?1) ? [ ?0, ?1 ] : [ ?1, ?0 ]
 end.transpose.map(&:join).map{ _1.to_i(2) }.inject(:*)
 
 def common_bit arr, n, op
-    counts = arr.map{ _1[n] }.join.yield_self{ [ _1.count(?0), _1.count(?1) ] }
-    counts[0].send(op, counts[1]) ? ?0 : ?1
+    arr.map{ _1[n] }.join.yield_self do
+        _1.count(?0).send(op, _1.count(?1)) ? ?0 : ?1
+    end
 end
 
 def calc_val arr, op
-    v1, v2 = 0, 0
-    idx = 0
-    loop do
+    for idx in 0...DATA.first.size
         cb = common_bit arr, idx, op
         arr = arr.select{ _1[idx] == cb }
-        break if arr.size < 2
-        idx += 1
-        raise :lol if idx > DATA.first.size
+        break if arr.size == 1
+        raise :lol unless arr.size > 1
     end
     arr.first.to_i(2)
 end
 
-PART2 = calc_val(DATA, :<=) * calc_val(DATA, :>)
+oxygen_generator_rating = -> arr { calc_val arr, :> }
+co2_scrubber_rating = -> arr { calc_val arr, :<= }
+
+PART2 = oxygen_generator_rating[DATA] * co2_scrubber_rating[DATA]
 
 puts 'Part 1: %s' % PART1
 puts 'Part 2: %s' % PART2
