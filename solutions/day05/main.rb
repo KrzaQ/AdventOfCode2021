66f88cb7a82109800946c30147e12330fe0f3fe0
KrzaQ <krzaq@krzaq.cc>
2021-12-05 06:47:02 +0100

    Day 05 code cleanup.

--- solutions/day05/main.rb
+++ solutions/day05/main.rb
@@ -4,20 +4,12 @@ DATA = File.read('data.txt').scan(/(\d+),(\d+) -> (\d+),(\d+)/)
     .map{ _1.map(&:to_i) }
 
 def make_line x1, y1, x2, y2
-    if x1 == x2
-        Range.new(*[y1, y2].sort).map{ [x1, _1] }
-    elsif y1 == y2
-        Range.new(*[x1, x2].sort).map{ [_1, y1] }
-    elsif (x2-x1).abs == (y2-y1).abs
-        xmin = [x1, x2].min
-        if (x2 > x1 and y2 > y1) or (x1 > x2 and y1 > y2)
-            ymin = [y1, y2].min
-            Range.new(*[x1, x2].sort).map{ [_1, ymin + _1 - xmin] }
-        else
-            ymax = [y1, y2].max
-            Range.new(*[x1, x2].sort).map{ [_1, ymax - _1 + xmin] }
-        end
-    end
+    x1, y1, x2, y2 = x2, y2, x1, y1 if x2 < x1
+    dx, dy = [x1, x2].reduce(:-).abs, [y1, y2].sort.reduce(:-).abs
+    return Range.new(*[y1, y2].sort).map{ [x1, _1] } if dx == 0
+    return (x1..x2).map{ [_1, y1] } if dy == 0
+    return (0..dx).map{ [x1 + _1, y1 + _1] } if y1 < y2
+    return (0..dx).map{ [x1 + _1, y1 - _1] } if y1 > y2
 end
 
 def overlapping &pred
@@ -28,8 +20,8 @@ def overlapping &pred
         .count{ _2.size > 1 }
 end
 
-part1 = -> { overlapping { _1 == _3 or _2 == _4 } }
-part2 = -> { overlapping { _1 == _3 or _2 == _4 or (_1-_3).abs == (_2-_4).abs} }
+PART1 = overlapping { _1 == _3 or _2 == _4 }
+PART2 = overlapping { _1 == _3 or _2 == _4 or (_1-_3).abs == (_2-_4).abs }
 
-puts 'Part 1: %s' % part1[]
-puts 'Part 2: %s' % part2[]
+puts 'Part 1: %s' % PART1
+puts 'Part 2: %s' % PART2
